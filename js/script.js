dec714a5c302d4d73b0d44a153d5556667044ba0
AzizVirus <aziz07becha@gmail.com>
2021-08-24 16:00:09 +0100

    Added scroll to top button

--- js/script.js
+++ js/script.js
@@ -8,13 +8,21 @@ $(document).ready(function () {
       }
   
       // scroll-up button show/hide script
-      if (this.scrollY > 1600) {
-        $(".scroll-up-btn").addClass("show");
-      } else {
-        $(".scroll-up-btn").removeClass("show");
-      }
+     
     });
-  
+  // ===== Scroll to Top ==== 
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(200);    // Fade in the arrow
+  } else {
+      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+  }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
     // slide-up script
     $(".scroll-up-btn").click(function () {
       $("html").animate({ scrollTop: 0 });
