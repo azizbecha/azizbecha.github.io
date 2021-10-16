78607f1cd7ba4b5d8cfe1efc125a7f3038b1edf6
AzizVirus <aziz07becha@gmail.com>
2021-10-16 19:35:45 +0100

    Clean the code

--- js/script.js
+++ js/script.js
@@ -1,45 +1,43 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-      // sticky navbar on scroll script
-      if (this.scrollY > 50) {
-        $(".navbar").addClass("sticky");
-      } else {
-        $(".navbar").removeClass("sticky");
-      }
-  
-      // scroll-up button show/hide script
-     
-    });
-  // ===== Scroll to Top ==== 
-$(window).scroll(function() {
-  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-      $('#return-to-top').fadeIn(200);    // Fade in the arrow
-  } else {
-      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-  }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-  $('body,html').animate({
-      scrollTop : 0                       // Scroll to top of body
-  }, 500);
-});
-    // slide-up script
-    $(".scroll-up-btn").click(function () {
-      $("html").animate({ scrollTop: 0 });
-      // removing smooth scroll on slide-up button click
-      $("html").css("scrollBehavior", "auto");
-    });
-  
-    $(".navbar .menu li a").click(function () {
-      // applying again smooth scroll on menu items click
-      $("html").css("scrollBehavior", "smooth");
-    });
-  
-    // toggle menu/navbar script
-    $(".menu-btn").click(function () {
-      $(".navbar .menu").toggleClass("active");
-      $(".menu-btn i").toggleClass("active");
-    });
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 50) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }     
+  });
 
+  // Scroll to top
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      $('#return-to-top').fadeIn(200);
+    } else {
+      $('#return-to-top').fadeOut(200);
+    }
+  });
+
+  $('#return-to-top').click(function() {
+    $('body,html').animate({
+      scrollTop : 0
+    }, 500);
   });
   
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
